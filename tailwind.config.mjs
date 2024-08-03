9514f139321f5c16e1f37126f12ca4f803beaf71
victormc13 <viktorsb14@gmail.com>
2024-08-03 16:38:08 -0500

    update: Adds boxShadow custom style for project articles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      boxShadow: {
+        custom: '0px 0px 20px rgba(0, 179, 134, 0.5)',
+      },
+    },
+  },
+  plugins: [],
+};
